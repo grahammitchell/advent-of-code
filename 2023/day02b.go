e38a51c45ebf9048853f476387d397b3d9b4a3b3
Graham Mitchell <graham@grahammitchell.com>
2023-12-02 21:02:18 -0600

    day 2!

---
+++ 2023/day02b.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	// "io/ioutil"
+	//"math"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type RGB struct {
+	red, green, blue, total int
+}
+
+type Game struct {
+	games []RGB
+}
+
+
+func read_input(filename string) []string {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	
+	var lines []string
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if scanner.Err() != nil {
+		panic(scanner.Err())
+	}
+	return lines
+}
+
+func deserialize(lines []string) []Game {
+	// Game 2: 7 red, 1 green, 4 blue; 13 red, 11 blue; 6 red, 2 blue; 9 blue, 9 red; 4 blue, 11 red; 15 red, 1 green, 3 blue
+	r, _ := regexp.Compile("Game \\d+: (.*)$")
+
+	games := make([]Game, 0)
+	for _, line := range lines {
+		// chop off "Game N: "
+		parts := r.FindStringSubmatch(line)
+		rest := parts[1]
+
+		// split up successive draws
+		draws := make([]RGB, 0)
+		for _, draw := range strings.Split(rest, "; ") {
+			// 7 red, 1 green, 4 blue
+			arrgb := strings.Split(draw, ", ")
+			rgb := splitDraw(arrgb)
+			draws = append(draws, rgb)
+		}
+		games = append(games, Game{draws})
+	}
+	return games
+}
+
+
+func splitDraw(arrgb []string) RGB {
+	vals := map[string]int{"red": 0, "green": 0, "blue": 0}
+	for _, color := range arrgb {
+		parts := strings.Split(color, " ")
+		count := atoi(parts[0])
+		vals[parts[1]] = count
+	}
+	total := vals["red"]+vals["green"]+vals["blue"]
+	return RGB{vals["red"], vals["green"], vals["blue"], total}
+
+}
+
+
+func atoi(s string) int {
+	n, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return n
+}
+
+func isIllegal(g Game) bool {
+	cubeCap := RGB{12, 13, 14, 39}
+	for _, rgb := range g.games {
+		if rgb.red > cubeCap.red || rgb.green > cubeCap.green || rgb.blue > cubeCap.blue {
+			return true
+		}
+	}
+	return false
+}
+
+func powerOfCubes(rgb RGB) int {
+	return rgb.red * rgb.green * rgb.blue
+}
+
+func getLeast(game Game) RGB {
+	var r, g, b int
+	for _, rgb := range game.games {
+		if rgb.red > r {
+			r = rgb.red
+		}
+		if rgb.green > g {
+			g = rgb.green
+		}
+		if rgb.blue > b {
+			b = rgb.blue
+		}
+	}
+	return RGB{r, g, b, r+g+b}
+}
+
+
+func main() {
+	lines := read_input("input/2")
+	games := deserialize(lines)
+	sum := 0
+	for _, game := range games {
+		sum += powerOfCubes(getLeast(game))
+	}
+	fmt.Println("Total: ", sum)
+}
